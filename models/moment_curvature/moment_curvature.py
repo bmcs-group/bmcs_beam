9a3e6b08bf6ee5c56ab3b3d0b99452b368a228f0
rch <rostislav.chudoba@rwth-aachen.de>
2020-09-15 18:09:55 +0200

    after zoom session sep 15.

--- models/moment_curvature/moment_curvature.py
+++ models/moment_curvature/moment_curvature.py
@@ -233,6 +233,14 @@ class MomentCurvature(tr.HasStrictTraits):
     def _get_kappa_norm(self):
         return self.kappa_cr
 
+    def get_kappa(self, M):
+        '''cut off the descending tails
+        '''
+        I_M = np.where(self.M_t[1:] - self.M_t[:-1] > 0)
+        M_I = self.M_t[I_M]
+        kappa_I = self.kappa_t[I_M]
+        return np.interp(M, M_I, kappa_I)
+
     def plot_norm(self, ax1, ax2):
         idx = self.idx
         ax1.plot(self.kappa_t / self.kappa_norm, self.M_t / self.M_norm)
@@ -248,12 +256,14 @@ class MomentCurvature(tr.HasStrictTraits):
         ax3.fill_betweenx(self.z_m, self.sig_tm[idx, :], 0, alpha=0.1)
         self._align_xaxis(ax2, ax3)
 
+    M_scale = tr.Float(1e+6)
+
     def plot(self, ax1, ax2):
         idx = self.idx
-        ax1.plot(self.kappa_t, self.M_t / (1e6))
+        ax1.plot(self.kappa_t, self.M_t / self.M_scale)
         ax1.set_ylabel('Moment [kN.m]')
         ax1.set_xlabel('Curvature [$m^{-1}$]')
-        ax1.plot(self.kappa_t[idx], self.M_t[idx] / (1e6), marker='o')
+        ax1.plot(self.kappa_t[idx], self.M_t[idx] / self.M_scale, marker='o')
         ax2.barh(self.z_j, self.N_s_tj[idx, :],
                  height=6, color='red', align='center')
         #ax2.plot(self.N_s_tj[idx, :], self.z_j, color='red')
@@ -285,104 +295,119 @@ class MomentCurvature(tr.HasStrictTraits):
         axes[0].set_xlim(extrema[0][0], b_new_t)
         axes[1].set_xlim(t_new_b, extrema[1][1])
 
+if __name__ == '__main__':
+    # Mobasher paper
 
-# Mobasher paper
+    # For info (left is paper notation, right is notation in this file):
+    # ------------------------------------------------------------------
+    # E = E_ct
+    # E_c = E_cc
+    # E_s = E_j
+    # eps_cr = eps_cr
+    # eps_cu = eps_cu
+    # eps_tu = eps_tu
+    # eps_cy = eps_cy
+    # mu = mu
 
-# For info (left is paper notation, right is notation in this file):
-# ------------------------------------------------------------------
-# E = E_ct
-# E_c = E_cc
-# E_s = E_j
-# eps_cr = eps_cr
-# eps_cu = eps_cu
-# eps_tu = eps_tu
-# eps_cy = eps_cy
-# mu = mu
+    # gamma = E_cc/E_ct
+    # omega = eps_cy/eps_cr
+    # lambda_cu = eps_cu/eps_cr
+    # beta_tu = eps_tu/eps_cr
+    # psi = eps_sy_j/eps_cr
+    # n = E_j/E_ct
+    # alpha = z_j/h
 
-# gamma = E_cc/E_ct
-# omega = eps_cy/eps_cr
-# lambda_cu = eps_cu/eps_cr
-# beta_tu = eps_tu/eps_cr
-# psi = eps_sy_j/eps_cr
-# n = E_j/E_ct
-# alpha = z_j/h
+    # r = A_s_c/A_s_t
 
-# r = A_s_c/A_s_t
+    # rho_g = A_j[0]/A_c # where A_j[0] must be tension steel area
+    # ------------------------------------------------------------------
 
-# rho_g = A_j[0]/A_c # where A_j[0] must be tension steel area
-# ------------------------------------------------------------------
+    ''' Parameters entry'''
+    # -----------------------
+    # Values from parametric study in paper p.11 to draw Fig. 7:
 
+    E_ct_ = 24000
+    eps_cr_ = 0.000125
+    gamma_ = 1
+    omega_ = 8.5
+    lambda_cu_ = 28
+    beta_tu_ = 160
+    psi_ = 16
+    n_ = 8.75
+    alpha_ = 0.9
 
-''' Parameters entry'''
-# -----------------------
-# Values from parametric study in paper p.11 to draw Fig. 7:
+    mu_ = 0.33
+    rho_g_ = 0.003
 
-E_ct_ = 24000
-eps_cr_ = 0.000125
-gamma_ = 1
-omega_ = 8.5
-lambda_cu_ = 28
-beta_tu_ = 160
-psi_ = 16
-n_ = 8.75
-alpha_ = 0.9
+    # not given in paper
+    r_ = 0.0
 
-mu_ = 0.33
-rho_g_ = 0.003
+    h_ = 666
+    zeta = 0.15  # WHY USING 0.15, 0.2 ... gives wrong results!!!
+    b_w = 50  # o = 0.1
+    b_f = 500
+    h_w = (1 - zeta) * h_
 
-# not given in paper
-r_ = 0.0
+    # Substituting formulas:
+    E_cc_ = gamma_ * E_ct_
+    eps_cy_ = omega_ * eps_cr_
+    eps_cu_ = lambda_cu_ * eps_cr_
+    eps_tu_ = beta_tu_ * eps_cr_
+    eps_sy_j_ = np.array([psi_ * eps_cr_, psi_ * eps_cr_])
+    E_j_ = np.array([n_ * E_ct_, n_ * E_ct_])
+    z_j_ = np.array([h_ * (1 - alpha_), alpha_ * h_])
 
-h_ = 666
-zeta = 0.15  # WHY USING 0.15, 0.2 ... gives wrong results!!!
-b_w = 50  # o = 0.1
-b_f = 500
-h_w = (1 - zeta) * h_
+    # Defining a variable b
+    b_z_ = sp.Piecewise(
+        (b_w, z < h_w),
+        (b_f, z >= h_w)
+    )
 
-# Substituting formulas:
-E_cc_ = gamma_ * E_ct_
-eps_cy_ = omega_ * eps_cr_
-eps_cu_ = lambda_cu_ * eps_cr_
-eps_tu_ = beta_tu_ * eps_cr_
-eps_sy_j_ = np.array([psi_ * eps_cr_, psi_ * eps_cr_])
-E_j_ = np.array([n_ * E_ct_, n_ * E_ct_])
-z_j_ = np.array([h_ * (1 - alpha_), alpha_ * h_])
+    A_c_ = sp.integrate(b_z_, (z, 0, h_))
+    A_s_t_ = rho_g_ * A_c_
+    A_s_c_ = r_ * A_s_t_
+    A_j_ = np.array([A_s_t_, A_s_c_])  # A_j[0] must be tension steel area
 
-# Defining a variable b
-b_z_ = sp.Piecewise(
-    (b_w, z < h_w),
-    (b_f, z >= h_w)
-)
+    ''' Creating MomentCurvature object '''
 
-A_c_ = sp.integrate(b_z_, (z, 0, h_))
-A_s_t_ = rho_g_ * A_c_
-A_s_c_ = r_ * A_s_t_
-A_j_ = np.array([A_s_t_, A_s_c_])  # A_j[0] must be tension steel area
+    mc = MomentCurvature(idx=25, n_m=100)
+    mc.h = h_
+    mc.b_z = b_z_
+    mc.model_params = {E_ct: E_ct_,
+                       E_cc: E_cc_,
+                       eps_cr: eps_cr_,
+                       eps_cy: eps_cy_,
+                       eps_cu: eps_cu_,
+                       mu: mu_,
+                       eps_tu: eps_tu_}
+    mc.z_j = z_j_
+    mc.A_j = A_j_
+    mc.E_j = E_j_
 
-''' Creating MomentCurvature object '''
+    if False:
+        # If plot_norm is used, use the following:
+        # mc.kappa_range = (0, mc.kappa_cr * 100, 100)
 
-mc = MomentCurvature(idx=25, n_m=100)
-mc.h = h_
-mc.b_z = b_z_
-mc.model_params = {E_ct: E_ct_,
-                   E_cc: E_cc_,
-                   eps_cr: eps_cr_,
-                   eps_cy: eps_cy_,
-                   eps_cu: eps_cu_,
-                   mu: mu_,
-                   eps_tu: eps_tu_}
-mc.z_j = z_j_
-mc.A_j = A_j_
-mc.E_j = E_j_
+        mc.kappa_range = (-0.00002, 0.00002, 100)
 
-# If plot_norm is used, use the following:
-# mc.kappa_range = (0, mc.kappa_cr * 100, 100)
+        # print('XXX', mc.kappa_cr)
 
-mc.kappa_range = (0, 0.00002, 100)
+        # Plotting
+        fig, ((ax1, ax2)) = plt.subplots(1, 2, figsize=(10, 5))
+        mc.plot(ax1, ax2)
+        plt.show()
 
-# print('XXX', mc.kappa_cr)
+    mc = MomentCurvature(idx=25, n_m=100)
 
-# Plotting
-fig, ((ax1, ax2)) = plt.subplots(1, 2, figsize=(10, 5))
-mc.plot(ax1, ax2)
-plt.show()
+    M_range = np.linspace(-40,70,100)
+    kappa = mc.get_kappa(M_range*1e+6)
+    print(kappa)
+
+    fig, ((ax1, ax2)) = plt.subplots(1, 2, figsize=(10, 5))
+    mc.plot(ax1, ax2)
+    plt.show()
+
+    # Plotting
+    fig, ax1 = plt.subplots(1, 1, figsize=(10, 5))
+    plt.plot(M_range, kappa)
+    plt.show()
