f786aef225893c50ce81682073faeb3f1de1f4b2
rch <rostislav.chudoba@rwth-aachen.de>
2020-10-27 17:19:59 +0100

    big renaming

--- api.py
+++
@@ -1,4 +0,0 @@
-from .beam_design.cross_section_layout import CrossSectionLayout, Matrix, Reinforcement
-from .beam_design.cross_section_shape import CrossSectionShapeBase, Rectangle, Circle, CustomShape, TShape
-from .beam_design.beam_design_ import BeamDesign
-from .moment_curvature.moment_curvature import MomentCurvature
--- main.py
+++ main.py
@@ -2,7 +2,4 @@
 import sys
 print(sys.path)
 
-import moment_curvature.moment_curvature
-
-import beam_design.cross_section_layout
 #from bmcs_beam.mxn.cross_section import MatrixCrossSection
