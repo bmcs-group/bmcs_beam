10a91029d97c93bab03ddbdc9da14d596449a39e
ishomam <ishomam@gmail.com>
2023-04-07 11:34:09 +0200

    Small fixes

--- bmcs_beam/bending/deflection_profile.py
+++ bmcs_beam/bending/deflection_profile.py
@@ -329,7 +329,7 @@ class DeflectionProfile(Model):
         d = mc.cross_section_shape_.H - z
         b = mc.cross_section_shape_.B
         L = self.beam_design.system_.L
-        f_ck = mc.cs_design.matrix_.f_cm
+        f_ck = mc.cs_design.matrix_.f_cm - 8
 
         C_Rk_c = 0.219
         gamma = 1
